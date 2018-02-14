3ce1a330d3ed50a60ea7ed444701e14abea32477
Job Joling <job@jobjol.com>
2018-02-14 21:23:23 +0100

    change working dir

--- pow.sh
+++ pow.sh
@@ -2,6 +2,7 @@
 DIR="$(pwd)"
 LANGCODE=$1
 
+# Check for mandantory argument.
 if [ -z $LANGCODE ];
 then
     echo `date`" - Missing mandatory arguments: Language code. "
@@ -15,13 +16,14 @@ then
     mkdir $DIR/translations
 fi
 
+# Check if a .module file exists in pwd
 if [ -e *.module ]; 
 then 
-    FILE="$DIR/translations/$LANGCODE.po"
-    echo $FILE
+    FILE="translations/$LANGCODE.po"
+    cd $DIR 
     if [ -f $FILE ]; 
     then
-	find $DIR -name "*.*" | xargs xgettext -o $FILE --keyword=t --language=PHP --force-po -j &&
+	find  $DIR -name "*.*" | xargs xgettext -o $FILE --keyword=t --language=PHP --force-po -j &&
 	echo `date`" - Updated existing language file moduleroot/translations/$LANGCODE.po"  
     else
 	find $DIR -name "*.*" | xargs xgettext -o $FILE --keyword=t --language=PHP --force-po  &&

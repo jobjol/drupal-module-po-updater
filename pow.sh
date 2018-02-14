10246f60f77b4293e4e97daadee1973b354114e9
Job Joling <job@jobjol.com>
2018-02-14 17:53:02 +0100

    Initial commit

---
+++ pow.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+DIR="$(pwd)"
+LANGCODE="${1,,}"
+
+if [ -z $LANGCODE ];
+then
+    echo `date`" - Missing mandatory arguments: Language code. "
+    echo `date`" - Usage: ./pow.sh [LANGCODE] . "
+    exit 1
+fi
+
+if [ -e *.module ]; 
+then 
+    FILE="$DIR/translations/$LANGCODE.po"
+    echo $FILE
+    if [ -f $FILE ]; 
+    then
+	find $DIR -name "*.*" | xargs xgettext -o $FILE --keyword=t --language=PHP --force-po -j &&
+	echo `date`" - Updated existing language file moduleroot/translations/LANGCODE.po"  
+    else
+	find $DIR -name "*.*" | xargs xgettext -o $FILE --keyword=t --language=PHP --force-po  &&
+	echo `date`" - New language file created: $FILE"
+    fi
+else $
+    echo `date`" - Call the script inside the module root folder. Exiting now."
+fi
